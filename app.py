16e0d0a2ebe4bffa6efe3071de1b45e3954e178d
fannysanchez21 <a385866@uach.mx>
2025-10-29 08:45:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 st.title("Ejemplo para usar sesion_state")
 
@@ -5,12 +6,15 @@ if 'count' not in st.session_state:
    st.session_state['count'] = 0
 
 if 'name' not in st.session_state:
-   st.session_state['nombre'] = ('nombre')
+   st.session_state['name'] = ''
    
 if st.button('Click me'):
    st.session_state['count'] += 1
 
 nombre = st.text_input('Escribe tu nombre')
 st.write(nombre)
-   
+
+st.session_state['name'] =
+st.text_input('Escribe tu nombre' , st.session_state['name'])
+
 st.write(st.session_state)
